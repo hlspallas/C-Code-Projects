83f428d3d95e2866dc9d6c36d8cb82bfe4a75cb2
Hunter Spallas <70778962+hlspallas@users.noreply.github.com>
2020-09-06 17:18:36 -0700

    Add files via upload

---
+++ Arduino/LED_Blink_Code.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void setup(){
+
+pinMode(LED_BUILTIN,OUTPUT);
+Serial.begin(9600);
+}
+
+
+float time;
+float time2;
+int i=0;
+float A;
+float B;
+float c;
+
+void loop(){
+while(i<20){
+    A = millis() / 1000.00;
+
+
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(random(50,1000));
+
+    serial.print("Total Time Elapsed:  ");
+    time = A;
+    serial.println(time,3);
+
+    digitalWrite(LED_BUILTIN,LOW);
+    delay(random(50,1000));
+
+    B = millis() / 1000.00;
+
+    c = B-A;
+
+    time2 = c;
+
+    serial.print("Blink Time Difference:  ");
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
